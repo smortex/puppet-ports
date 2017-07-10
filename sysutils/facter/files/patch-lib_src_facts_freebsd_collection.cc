99589ffbeee83f0c4c4a0b42365dae5f76ee4090
Romain Tartière <romain@blogreen.org>
2017-07-10 18:55:47 +0200

    Integrate zfs/zpool facts fixes

--- sysutils/facter/files/patch-lib_src_facts_freebsd_collection.cc
+++ sysutils/facter/files/patch-lib_src_facts_freebsd_collection.cc
@@ -1,6 +1,6 @@
 --- lib/src/facts/freebsd/collection.cc.orig	2017-06-27 17:42:22 UTC
 +++ lib/src/facts/freebsd/collection.cc
-@@ -8,8 +8,10 @@
+@@ -8,8 +8,12 @@
  #include <internal/facts/posix/kernel_resolver.hpp>
  #include <internal/facts/posix/ssh_resolver.hpp>
  #include <internal/facts/posix/timezone_resolver.hpp>
@@ -9,10 +9,12 @@
  #include <internal/facts/freebsd/networking_resolver.hpp>
 +#include <internal/facts/freebsd/virtualization_resolver.hpp>
 +#include <internal/facts/freebsd/memory_resolver.hpp>
++#include <internal/facts/freebsd/zfs_resolver.hpp>
++#include <internal/facts/freebsd/zpool_resolver.hpp>
  
  using namespace std;
  
-@@ -18,7 +20,7 @@ namespace facter { namespace facts {
+@@ -18,7 +22,7 @@ namespace facter { namespace facts {
      void collection::add_platform_facts()
      {
          add(make_shared<posix::kernel_resolver>());
@@ -21,12 +23,14 @@
          add(make_shared<freebsd::networking_resolver>());
          add(make_shared<bsd::uptime_resolver>());
          add(make_shared<bsd::filesystem_resolver>());
-@@ -28,6 +30,8 @@ namespace facter { namespace facts {
+@@ -28,6 +32,10 @@ namespace facter { namespace facts {
          add(make_shared<glib::load_average_resolver>());
          add(make_shared<freebsd::processor_resolver>());
          add(make_shared<freebsd::dmi_resolver>());
 +        add(make_shared<freebsd::virtualization_resolver>());
 +        add(make_shared<freebsd::memory_resolver>());
++        add(make_shared<freebsd::zfs_resolver>());
++        add(make_shared<freebsd::zpool_resolver>());
      }
  
  }}  // namespace facter::facts
